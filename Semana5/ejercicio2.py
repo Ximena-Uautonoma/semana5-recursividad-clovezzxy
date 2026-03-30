b794f8e10b4b38e64610a9ce7fa5c5661585ac16
clovezzxy <gabrialonsogomez2810@gmail.com>
2026-03-30 15:16:55 -0300

    ej2 terminado

--- Semana5/ejercicio2.py
+++ Semana5/ejercicio2.py
@@ -8,14 +8,16 @@ Debe implementar:
 """
 
 def suma_ciclo(n):
-    """
-    Retorna la suma de los primeros n números usando un ciclo.
-    """
-    pass
+ def suma_ciclo(n):
+    suma = 0
+    for i in range(n):
+        suma += (i + 1)
+    print(suma)
 
 
 def suma_recursiva(n):
-    """
-    Retorna la suma de los primeros n números usando recursividad.
-    """
-    pass
+    if n == 1: 
+        return 1
+    else:
+        return suma_recursiva(n - 1) + n
+print(suma_recursiva(5))
